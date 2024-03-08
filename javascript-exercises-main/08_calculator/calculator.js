e7aefaadcf820671abbfc7b4b75477e9a3431d7f
Nirneen <49162323+Nirneen@users.noreply.github.com>
2024-03-08 17:30:24 +0300

    complete js ex

--- javascript-exercises-main/08_calculator/calculator.js
+++ javascript-exercises-main/08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a ,b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, currentItem) => total + currentItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, currentItem) => total * currentItem, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let numPower = 1;
+  for(let i=0;i<b;i++){
+    numPower *= a;
+  }
+  return numPower
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  if(num === 0) {return result}
+  for(let i=1;i<=num;i++){
+    result *= i;
+  }
+  return result
 };
 
 // Do not edit below this line
--- javascript-exercises-main/10_fibonacci/fibonacci.js
+++ javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index < 0) {return "OOPS"}
+    if((index == 1) || (index == 2)) {return 1}
+    if(index == 0) {return 0}
+    let answear = 1
+    let previos = 1
+    for(let i=3;i<=index;i++){
+        let temp = answear;
+        answear = answear + previos;
+        previos = temp;
+    }
+    return answear
 };
 
 // Do not edit below this line
